26e8dd9fa6165b7f7de8befb653b4071d1a56df3
Ricardo <ricardodjsc@gmail.com>
2023-11-26 17:24:22 -0400

    Add react-router-dom and tailwind

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Home</h1>
+      <p>Welcome to the home page!</p>
+    </div>
+  )
+}
+
+export default Home;
