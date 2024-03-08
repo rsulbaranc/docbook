bdeeaf2d4a8a498b1b0a665276ad9e651b433a51
Ricardo <ricardodjsc@gmail.com>
2024-03-07 21:35:42 -0400

    Refactor sidebar navigation routes

--- frontend/src/components/navbar/Sidebar.jsx
+++ frontend/src/components/navbar/Sidebar.jsx
@@ -3,9 +3,10 @@ import logo from "../../assets/img/logo_alternativo_docbook.png"
 import { AiOutlineRight, AiOutlineHome, AiOutlineShoppingCart, AiOutlineBarChart , AiOutlineSetting  } from "react-icons/ai";
 import { MdOutlineLogout } from "react-icons/md";
 import { NavLink } from 'react-router-dom'
-import { publicRoutes, patientRoutes, doctorRoutes } from "./navigation";
+import { useRoutesInfo } from "./navigation";
 import { useAuth } from "../../context/AuthContext";
 import React from "react";
+import { FaSignOutAlt } from "react-icons/fa";
 
 
 export const Sidebar = ({sidebarOpened, setSidebarOpened}) => {
@@ -29,6 +30,8 @@ const openSidebar = () => {
   setSidebarOpened(!sidebarOpened)
 }
 
+const routes = useRoutesInfo(user.profile);
+
   return (
     <div className='contain'>
       <button onClick={openSidebar} className={sidebarOpened ? 'sidebar-btn active' : 'sidebar-btn'}>
@@ -40,7 +43,7 @@ const openSidebar = () => {
         </div>
         <h2 className={sidebarOpened ? 'name active text-2xl font-bold' : 'name'}>DocBook</h2>
       </div>
-      {(user && user.profile === 'patient' ? patientRoutes : user && user.profile === 'doctor' ? doctorRoutes : publicRoutes).map(({icon, path, label}) => (
+      {routes.map(({icon, path, label}) => (
         <div className='routesContainer' key={label}>
           <NavLink to={path} className="routes" activeClassName="active">
             <div className='routesIcon'>
@@ -53,7 +56,7 @@ const openSidebar = () => {
         </div>
       ))}
       <div className='divider'></div>
-      {optionsArray.map(({icon, path, label}) => (
+      {/* {optionsArray.map(({icon, path, label}) => (
         <div className='routesContainer' key={label}>
           <NavLink to={path} className="routes" activeClassName="active">
             <div className='routesIcon'>
@@ -64,7 +67,16 @@ const openSidebar = () => {
             }
           </NavLink>
         </div>
-      ))}
+      ))} */}
+
+      <div className="routesContainer">
+        <div className="routes" style={{cursor: "pointer"}} onClick={signout}>
+          <div className="routesIcon">
+            <FaSignOutAlt />
+          </div>
+          { sidebarOpened && <span>Cerrar Sesión</span>}
+        </div>
+      </div>
       <div className='divider'></div>
     </div>
   )
--- frontend/src/components/navbar/navigation.jsx
+++ frontend/src/components/navbar/navigation.jsx
@@ -1,27 +1,40 @@
-import React from 'react'
 import { AiOutlineHome } from 'react-icons/ai';
+import { CgProfile } from "react-icons/cg";
+import { FaNotesMedical } from "react-icons/fa";
+import { FaUserDoctor } from "react-icons/fa6";
 
-export const navigation = () => {
 
-  const publicRoutes = [
-    { label: "Login", path: "/login", current: false },
-    { label: "Register", path: "/register", current: false },
-  ];
-  
-  const patientRoutes = [
-    { label: "Home", path: "/", current: true, icon: <AiOutlineHome/>},
-    { label: "Profile", path: "/profile", current: false }
-  ];
-  
-  const doctorRoutes = [
-    { label: "Home", path: "/dashboard", current: false},
-    { label: "Profile", path: "/profile", current: false },
-    { label: "Create Record", path: "/createRecord", current: false },
-    { label: "Register Patient", path: "/registerPatient", current: false },
-  ]
-  
+const publicRoutes = [
+  { label: "Login", path: "/login", current: false },
+  { label: "Register", path: "/register", current: false },
+];
 
-  return (
-    <div>navigation</div>
-  )
+const patientRoutes = [
+  { label: "Home", path: "/", current: true, icon: <AiOutlineHome/>},
+  { label: "Profile", path: "/profile", current: false, icon: <CgProfile />}
+];
+
+const doctorRoutes = [
+  { label: "Home", path: "/dashboard", current: false , icon: <AiOutlineHome/>},
+  { label: "Profile", path: "/profile", current: false, icon: <CgProfile /> },
+  { label: "Create Record", path: "/createRecord", current: false, icon: <FaNotesMedical />},
+  { label: "Register Patient", path: "/registerPatient", current: false, icon: <FaUserDoctor />},
+]
+
+export const useRoutesInfo = (userProfile) => {
+
+  let routes;
+
+  switch(userProfile) {
+    case 'patient':
+      routes = patientRoutes;
+      break;
+    case 'doctor':
+      routes = doctorRoutes;
+      break;
+    default:
+      routes = publicRoutes;
+  }
+
+  return routes;
 }
