aa687299f314acf20c718bdd2e064bd2b6a0df42
Ricardo <ricardodjsc@gmail.com>
2024-03-20 19:01:19 -0400

    Fix import and add admin user creation logic

--- backend/src/controllers/bo/Security.js
+++ backend/src/controllers/bo/Security.js
@@ -1,4 +1,4 @@
-import  {pool}  from "../../db.js";
+import  { pool }  from "../../db.js";
 import bcrypt from "bcrypt";
 import querys from "../../utils/querys.json" assert { type: "json" };
 
@@ -11,14 +11,28 @@ let Security = class {
     }
 
     init = async () => {
+        let client = await pool.connect();
         try {
-            const result = await this.db.query(this.querys.security.checkAdminExists);
+            const result = await client.query(this.querys.security.checkAdminExists);
             const adminExists = result.rows[0].count > 0;
-
+           
             if (!adminExists) {
-                const hashedPassword = await this.bycrypt.hash('admin123', 10);
-                await this.db.query(this.querys.auth.register, ['administrador', 'admin@admin.com', hashedPassword, 'admin']);
-                console.log("Admin user created");
+                try {
+                    const hashedPassword = await this.bycrypt.hash('admin123', 10);
+                    await client.query("BEGIN");
+                    const profileResult = await client.query(this.querys.security.createAdminProfile);
+                    const userResult = await client.query(this.querys.security.createAdminUser, ['administrador', hashedPassword]);
+                    const profileID = profileResult.rows[0].profile_id;
+                    const userID = userResult.rows[0].user_id;
+                    await client.query(this.querys.security.createProfileUser, [profileID, userID]);
+                    await client.query("COMMIT");
+                    console.log("Admin user created");
+                } catch (error) {
+                    console.log(error);
+                    await client.query("ROLLBACK");
+                }
+                //console.log(`el id del perfil es: ${id1} y el del usuario ${id2}`);
+                //await this.db.query(this.querys.auth.register, ['administrador', 'admin@admin.com', hashedPassword, 'admin']);
             }
         } catch (error) {
             console.log(error);
